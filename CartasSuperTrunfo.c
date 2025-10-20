739425ae495e5b5db25db2aaab49abe5477895e7
gabrielycalistrosantos-web <gabrielycalistrosantos@gmail.com>
2025-10-20 20:58:16 +0000

    Criei o desafio CartasSuperTrunfo em C.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,78 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
+// Cada carta deve conter os seguintes atributos:
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado[20];
+  char codigo[5];
+  char cidade[20];
+  int populacao;
+  float area;
+  float pib;
+  int turismo;
 
-  // Área para entrada de dados
 
-  // Área para exibição dos dados da cidade
+printf("Bem vindo(a) ao jogo Super-Trunfo. Vamos iniciar coletando dados de suas cartas. \n");
+
+// dados da primeira carta:
+
+printf("Vamos cadastrar os dados da sua primeira carta. \n");
+
+printf("Estado da carta 1:\n");
+scanf(" %s", estado);
+
+printf("Código da carta 1:\n");
+scanf("%s", codigo);
+
+printf("Nome da cidade carta 1:\n");
+scanf("%s", cidade);
+
+printf("População total carta 1:\n");
+scanf("%d", &populacao);
+
+printf("Tamanho da área em Km² carta 1:\n");
+scanf("%f", &area);
+
+printf("Produto Interno Bruto - PIB carta 1:\n");
+scanf("%f", &pib);
+
+printf("Quantidade de pontos turísticos carta 1:\n");
+scanf("%d", &turismo);
+printf("\n");
+
+// dados da segunda carta:
+
+printf("Agora vamos cadastrar os dados da sua segunda carta. \n");
+
+printf("Estado da carta 2:\n");
+scanf(" %s", estado);
+
+printf("Código da carta 2:\n");
+scanf("%s", codigo);
+
+printf("Nome da cidade carta 2:\n");
+scanf("%s", cidade);
+
+printf("População total carta 2:\n");
+scanf("%d", &populacao);
+
+printf("Tamanho da área em Km² carta 2:\n");
+scanf("%f", &area);
+
+printf("Produto Interno Bruto - PIB carta 2:\n");
+scanf("%f", &pib);
+
+printf("Quantidade de pontos turísticos carta 2:\n");
+scanf("%d", &turismo);
+printf("\n\n");
+
+printf("Carta 1: \n Estado: %s \n Código: %s \n Nome da Cidade: %s \n", estado, codigo, cidade);
+printf(" População: %d \n Área: %f \n PIB: %f bilhões \n Número de pontos turísticos: %d  \n", populacao, area, pib, turismo);
+
+printf("\n");
+
+printf("Carta 2: \n Estado: %s \n Código: %s \n Nome da Cidade: %s \n", estado, codigo, cidade);
+printf(" População: %d \n Área: %f \n PIB: %f bilhões \n Número de pontos turísticos: %d \n", populacao, area, pib, turismo);
 
 return 0;
 } 
